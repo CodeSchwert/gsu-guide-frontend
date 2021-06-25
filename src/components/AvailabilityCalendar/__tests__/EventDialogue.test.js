254d72168a10da6c51509fbd4c9baaf9cfbc8665
CodeSchwert <2661899+CodeSchwert@users.noreply.github.com>
2021-06-25 13:05:27 +1200

    Update failing tests, use regex selectors.

--- src/components/AvailabilityCalendar/__tests__/EventDialogue.test.js
+++ src/components/AvailabilityCalendar/__tests__/EventDialogue.test.js
@@ -13,9 +13,9 @@ describe('Event Dialogue', () => {
     const endDate = new Date('2021-06-20T07:30:00');
 
     render(<EventDialogue open={true} start={startDate} end={endDate} />);
-    
-    const startTimeInput = screen.getByLabelText('Start Time');
-    const endTimeInput = screen.getByLabelText('End Time');
+
+    const startTimeInput = screen.getByLabelText(/Start Time/i);
+    const endTimeInput = screen.getByLabelText(/End Time/i);
     expect(startTimeInput.value).toEqual('06:00 AM');
     expect(endTimeInput.value).toEqual('07:30 AM');
   });
@@ -23,14 +23,26 @@ describe('Event Dialogue', () => {
   it('should render description (title prop) correctly', () => {
     const title = 'Foo bar lorem ipsum';
     render(<EventDialogue open={true} title={title} />);
-    const descriptionInput = screen.getByLabelText('Description');
+    const descriptionInput = screen.getByLabelText(/Description/i);
     expect(descriptionInput.value).toEqual(title);
   });
 
   it('should fire submit handler', () => {
+    const title = 'Foo bar lorem ipsum';
+    const startDate = new Date('2021-06-20T06:00:00');
+    const endDate = new Date('2021-06-20T07:30:00');
+
     const handleSubmit = jest.fn();
-    render(<EventDialogue open={true} handleSubmit={handleSubmit} />);
-    const submitButton = screen.getByText('Submit');
+    render(
+      <EventDialogue
+        open={true}
+        title={title}
+        start={startDate}
+        end={endDate}
+        handleSubmit={handleSubmit}
+      />
+    );
+    const submitButton = screen.getByText(/^Submit$/i);
     fireEvent.click(submitButton);
     expect(handleSubmit).toBeCalled();
   });
